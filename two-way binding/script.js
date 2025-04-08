ac9ae5eedc830e026a9789c4551c90e81898bef7
Thulasiraman333 <36729169+Thulasiraman333@users.noreply.github.com>
2025-04-08 21:36:31 +0530

    Two way binding table format

---
+++ two-way binding/script.js
@@ -0,0 +1,9 @@
+var myApp = angular.module("myModule", []).
+  controller("myController", function ($scope) {
+    var employee = {
+      firstName: "Ben",
+      lastName: "Hastings",
+      gender: "Male"
+    };
+    $scope.employee = employee;
+  })
